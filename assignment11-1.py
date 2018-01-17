50e47a58c4c18a325ddf5b9b4e73115b2b0d753d
another1s <164536681@qq.com>
2018-01-16 22:03:56 -0600

    Add files via upload

--- assignment11-1.py
+++ assignment11-1.py
@@ -1,24 +1,2 @@
 import re
 
-file_name=input("please input your file name:")
-handle=open(file_name)
-
-lst=list()
-lst.append('123')
-sum=0
-num=0
-for line in handle:
-    line=line.strip()
-    number=re.findall('[0-9]+',line)
-   # print(number)
-    if len(number)!=0 :
-        count=0                                  #此处遍历每一行得到的是存有所有数字的list
-        while count<len(number):
-          num=num+int(number[count])
-          count=count+1
-    else :
-        continue
-print(num)
-print(type(lst[0]))
-sum
-#D:\pythoncode\11-1\text.txt
